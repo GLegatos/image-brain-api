dc7e228fa4dfd55b639893739bdd0f3c29dcce70
GLegatos <GLegatos@gmail.com>
2020-10-20 19:47:15 -0400

    updating database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-triangular-32477',
       user : 'i92fender',
       password : '',
       database : 'smart-brain'
