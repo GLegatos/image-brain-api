59caedce8eb33e7581e5723e5d09ae662f087bc9
GLegatos <GLegatos@gmail.com>
2020-10-20 19:51:50 -0400

    connecting database

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-triangular-32477',
-      user : 'i92fender',
-      password : '',
-      database : 'smart-brain'
+      host : process.env.DATABASE_URL,
+      ssl: true,
     }
   });
 
