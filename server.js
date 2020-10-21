acc721c978ab83d34a4201cd3c80f9624e32601c
GLegatos <GLegatos@gmail.com>
2020-10-20 20:02:44 -0400

    debugged connection

--- server.js
+++ server.js
@@ -10,11 +10,15 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+
 const db = knex({
     client: 'pg',
     connection: {
       connectString : process.env.DATABASE_URL,
-      ssl: true,
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
   });
 
